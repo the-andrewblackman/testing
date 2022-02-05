4134ad950666a1fc325f5955f09c8e6df154b198
ChromeCowboy <cab886@gmail.com>
2022-02-05 10:39:22 -0800

    test
    test

---
+++ Coding Challenge 12_6/absoluteValue.java
@@ -0,0 +1,12 @@
+public class absoluteValue {
+    public static void main(String[] args) {
+
+    }
+
+    public int absolute(int a, int b) {
+        // get the absolute value of the difference of two integers.
+        int abs = Math.abs(a - b);
+        // return the absolute value of stored integer.
+        return abs;
+    }
+}
